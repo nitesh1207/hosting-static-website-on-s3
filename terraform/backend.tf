a11b9c1603a4220f9fe148ebb45c53123652fe95
nitesh1207 <kumarniteshjha61323@gmail.com>
2025-01-15 07:26:38 +0000

    adding cred

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 
 resource "aws_dynamodb_table" "terraform_lock" {
-  name           = "terraform-lock-table"  # Name of the table
+  name           = "terraform-niteshlock-table"  # Name of the table
   billing_mode   = "PAY_PER_REQUEST"       # Use on-demand mode (no need to specify read/write capacity)
   hash_key       = "LockID"                # Hash key for the table
   attribute {
