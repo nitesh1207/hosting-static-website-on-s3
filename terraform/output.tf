340a101715549e514014b4db8660234df2b702fc
nitesh1207 <kumarniteshjha61323@gmail.com>
2025-01-15 06:43:20 +0000

    adding cred

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,3 +1,1 @@
-output "website_endpoint" {
-  value = aws_s3_bucket.static_website.website_endpoint
-}
+
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,8 +1,3 @@
-variable "bucket_name" {
-  description = "Name of the S3 bucket"
-  type        = string
-}
-
 variable "region" {
   description = "AWS Region"
   type        = string
